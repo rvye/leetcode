747c10a8189a652b3c458603aa76ffa6d3421acb
rye <rvyeinq@gmail.com>
2023-03-06 08:47:06 -0600

    Update AddTwoNumbers.py

--- Python/AddTwoNumbers.py
+++ Python/AddTwoNumbers.py
@@ -1,44 +1,9 @@
-# Kinda right, but WIP
-
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        l1 = [9,9,9,9,9,9,9]
-        l2 = [9,9,9,9]
+        l1 = [2, 4, 3]
+        l2 = [5, 6, 4]
 
-        
 
-        if len(l1) == 3 or len(l2) == 3:
-            al1 = f"{l1[2]}" + f"{l1[1]}" + f"{l1[0]}"
-            al2 = f"{l2[2]}" + f"{l2[1]}" + f"{l2[0]}"
-
-            al1 = int(al1)
-            al2 = int(al2)
-
-            output = al1 + al2
-            output = [int(x) for x in str(output)][::-1]
-            
-            print(output)
-        
-        if len(l1) == 1 or len(l2) == 1:
-            al1 = f"{l1[0]}"
-            al2 = f"{l2[0]}"
-
-            al1 = int(al1)
-            al2 = int(al2)
-
-            output = al1 + al2
-            output = [int(x) for x in str(output)][::-1]
-            
-            print(output)
-        if len(l1) == 7 or len(l2) == 4:
-            al1 = f"{l1[6]}" + f"{l1[5]}" + f"{l1[4]}" + f"{l1[3]}" + f"{l1[2]}" + f"{l1[1]}" + f"{l1[0]}"
-            al2 = f"{l2[3]}" + f"{l2[2]}" + f"{l2[1]}" + f"{l2[0]}"
-
-            al1 = int(al1)
-            al2 = int(al2)
-
-            output = al1 + al2
-            output = [int(x) for x in str(output)][::-1]
-            
-            print(output)
-        
+        for i in range(3):
+            o = l1[i] + l2[i]
+            o = [{l1[i]} + {l2[i]}]
