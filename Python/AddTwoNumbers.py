9d63c3bb7cb6dd6fac0cb0b297d72d120e0a293c
rye <rvyeinq@gmail.com>
2023-03-06 10:08:09 -0600

    Update AddTwoNumbers.py

--- Python/AddTwoNumbers.py
+++ Python/AddTwoNumbers.py
@@ -3,7 +3,13 @@ class Solution:
         l1 = [2, 4, 3]
         l2 = [5, 6, 4]
 
+        z1 = int({l1[2]}) + int({l2[2]})
+        z2 = int({l1[1]}) + int({l2[1]})
+        z3 = int({l1[0]}) + int({l2[0]})
+    
+        z1 = str(z1)
+        z2 = str(z2)
+        z3 = str(z3)
 
-        for i in range(3):
-            o = l1[i] + l2[i]
-            o = [{l1[i]} + {l2[i]}]
+        out = f"{z1}{z2}{z3}"
+        print(out)
