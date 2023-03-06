96503a9adfc31b6edb9246c9746cb1301b465417
rye <rvyeinq@gmail.com>
2023-03-06 10:11:06 -0600

    Update AddTwoNumbers.py

--- Python/AddTwoNumbers.py
+++ Python/AddTwoNumbers.py
@@ -3,13 +3,11 @@ class Solution:
         l1 = [2, 4, 3]
         l2 = [5, 6, 4]
 
-        z1 = int({l1[2]}) + int({l2[2]})
-        z2 = int({l1[1]}) + int({l2[1]})
-        z3 = int({l1[0]}) + int({l2[0]})
-    
-        z1 = str(z1)
-        z2 = str(z2)
-        z3 = str(z3)
+        z1 = str(int(l1[2]) + int(l2[2]))
+        z2 = str(int(l1[1]) + int(l2[1]))
+        z3 = str(int(l1[0]) + int(l2[0]))
 
-        out = f"{z1}{z2}{z3}"
-        print(out)
+
+        print(z1)
+        print(z2)
+        print(z3)
