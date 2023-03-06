481ae55b574df43407efffcfabb451c67f3ec94e
rye <rvyeinq@gmail.com>
2023-03-06 10:45:01 -0600

    Update AddTwoNumbers.py

--- Python/AddTwoNumbers.py
+++ Python/AddTwoNumbers.py
@@ -1,13 +1,34 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+        # Testcase One
+
         l1 = [2, 4, 3]
         l2 = [5, 6, 4]
 
-        z1 = str(int(l1[2]) + int(l2[2]))
-        z2 = str(int(l1[1]) + int(l2[1]))
-        z3 = str(int(l1[0]) + int(l2[0]))
+        z1 = int(l1[2]) + int(l2[2])
+        z2 = int(l1[1]) + int(l2[1])
+        z3 = int(l1[0]) + int(l2[0])
 
 
-        print(z1)
-        print(z2)
-        print(z3)
+        if z2 >= 10:
+            z1 += 1
+            z2 = 0
+        elif z3 >= 10:
+            z3 = 0
+
+        output = str(z1) + str(z2) + str(z3)
+
+        output = [int(x) for x in str(output)][::-1]
+
+        # print(output)
+
+        # Testcase Two
+
+        k1 = [0]
+        k2 = [0]
+
+        k = str(int(k1[0]) + int(k2[0]))
+        k = [int(x) for x in str(k)][::-1]
+
+        (k)
